b6920206b567a3aa648e51ee3c4eb3feb0064177
ankitsingh2403 <ankitsingh0075455@gmail.com>
2025-04-05 23:54:03 +0530

    modified:   src/components/Shimmer.jsx 	modified:   src/index.css

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -17,6 +17,8 @@ const Shimmer = () =>{
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
+        
+      
     </div>
 }
 
